f28a5368f5eff9c4c445232fc52961ff86d8aa11
omarahmad <omarahmadmohamed@gmail.com>
2022-07-26 13:20:25 +0200

    add trivia screen

---
+++ lib/screens/trivia_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tourism_new/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+class TriviaScreen extends StatelessWidget {
+  const TriviaScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
